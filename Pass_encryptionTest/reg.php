4a90efeaddfa89003bfcc544257cc604abe1fef1
JGustavo11 <jgustavo11@mail.fresnostate.edu>
2017-02-14 12:38:38 -0800

    not working, but its the reg.php file

---
+++ Pass_encryptionTest/reg.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html>
+<head>
+<title>Register Now</title>
+</head>
+<body>
+
+<h3>Registration Form</h3>
+<form name="form1" action="" method="post">
+Username: <input type=text name="u1"><br/>
+Password: <input type=password name="p1"><br/>
+
+<input type="submit" name="submit1" name='Register'>
+</form>
+
+<?php
+if(isset($_POST["submit1"]))
+{
+$pwd=md5($_POST["p1"]);
+mysql_connect("localhost","root","");
+mysql_select_db("users");
+mysql_query("insert into registration values('$_POST[users]','$pwd')");
+    
+    
+}
+
+
+
+
+?>
+
+</body>
+</html>
