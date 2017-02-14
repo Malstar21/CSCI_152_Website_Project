2527fb219e4932e32cbe87dd83319cff14984fa6
JGustavo11 <jgustavo11@mail.fresnostate.edu>
2017-02-14 12:40:00 -0800

    member area when login. Not working

---
+++ Pass_encryptionTest/mber.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+if(isset($_SESSION['user']))
+{
+?>
+<!doctype html>
+<html>
+<head>
+<title>Member</title>
+</head>
+<body>
+<?php echo "Welcome, ".$_SESSION['user']."!";?>
+<br />
+<h3>Welcome to the member aread!</h3>
+<p><a href="logout.php">Logout</a></p>
+</body>
+</html>
+<?php
+}
+else
+{
+echo "Please Login First! <a href='login.php'>Login</a>";
+    
+}
+?>
