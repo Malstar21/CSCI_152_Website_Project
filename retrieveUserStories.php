a404ea41c67088b91833371f73dca0795a5c55fd
Malek Elzaatari <malek.02@hotmail.com>
2017-02-15 12:53:18 -0800

    added echo and comments to code

--- retrieveUserStories.php
+++ retrieveUserStories.php
@@ -1,23 +1,27 @@
 <?php
-	$dbhost = "localhost";
-	$user = "root";
-	$password = "";
-	$db = "seem website";
+	$conn = mysql_connect("localhost", "root", "");
+	mysql_select_db("seem website");
 
-	$conn = mysql_connect($dbhost, $user, $password);
-	mysql_select_db($db);
+	// if connection failed display connection failed
+	if ($conn == false)
+		die("Connection failed");
 
-	if ($conn == false) {
-	die("Connection failed");
-	}
-	
-	$id =1;
-	$sql = "SELECT id FROM makeastory WHERE id=$id";
+	// this will be where we get the unique user id and pull there stories
+	// from makeastory database
+	$id =0;
+	$sql = "SELECT title, id, userKey FROM makeastory WHERE userKey=$id";
 
 	$result = mysql_query($sql);
 
+	// this will display titles of stories that belong to that user account
+	// using example link for now
+	$num = 1;
 	while($row = mysql_fetch_array($result))
 	{
-		echo $row['id'];
+		echo $num;
+		echo ". ";
+		echo "<a href='http://localhost/displaytemplate.php?rowid={$row['id']}'> <b> {$row['title']} </b> </a>";
+		echo "<br>";
+		$num++;
 	}
-  ?>
+?>
