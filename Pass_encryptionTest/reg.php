9daf4931a8ae8bbcd645a20a51067df9b1aada00
elvin-rivera23 <elvin_rivera@mail.fresnostate.edu>
2017-03-27 11:54:19 -0700

    updated the registration form and created the form for the types of inputs, not fully working

--- Pass_encryptionTest/reg.php
+++ Pass_encryptionTest/reg.php
@@ -1,33 +1,39 @@
-<!doctype html>
-<html>
-<head>
-<title>Register Now!</title>
-</head>
-<body>
-
-<h3>Registration Form</h3>
-<form name="form1" action="" method="post">
-Username: <input type=text name="u1"><br/>
-Password: <input type=password name="p1"><br/>
-
-<input type="submit" name="submit1" name='Register'>
-</form>
-
 <?php
-if(isset($_POST["submit1"]))
-{
-$pwd=md5($_POST["p1"]);
-mysql_connect("localhost","root","");
-mysql_select_db("md5_password");
-mysql_query("insert into registration values('$_POST[u1]','$pwd')");
+session_start();
+$_SESSION['message'] = '';
+$mysqli = new mysqli('localhost','root','','regusers');
+
+//the form has been submitted with post
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
     
+    //two passwords are equal to each other
+    if ($_POST['password'] == $_POST['confirmpassword']) {
+        
+        //post variable for username 
+        $username = $mysqli->real_escape_string($_POST['username']);
+        //post variable for email
+        $email = $mysqli->real_escape_string($_POST['email']);
+        //md5 and post variable for password 
+        $password = md5($_POST['password']); 
+        //post variable for avatar
+        //store files to images folder
+        $avatar_path = $mysqli->real_escape_string('images/'.$_FILES['avatar']['name']);
+
     
 }
 
 
-
-
-?>
-
-</body>
-</html>
+<link rel="stylesheet" href="registrationform.css" type="text/css">
+<div class="body-content">
+  <div class="module">
+    <h1>Welcome! Register Now!</h1>
+    <form class="form" action="registrationform.php" method="post" enctype="multipart/form-data" autocomplete="off">
+      <div class="alert alert-error"><?= $_SESSION['message'] ?></div> 
+      <input type="text" placeholder="UserName" name="username" />
+      <input type="email" placeholder="Email" name="email" />
+      <input type="password" placeholder="Password" name="password" />  
+      <div class="avatar"><label>Select your avatar: </label><input type="file" name="avatar" accept="image/*" required /></div>
+      <input type="submit" value="Register" name="register" class="btn btn-block btn-primary" />
+    </form>
+  </div>
+</div>
