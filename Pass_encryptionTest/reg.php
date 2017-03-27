59d57ce025caf8ce881a9b5c7f0490ed24821357
JGustavo11 <jgustavo11@mail.fresnostate.edu>
2017-03-27 12:16:44 -0700

    found missing parts

--- Pass_encryptionTest/reg.php
+++ Pass_encryptionTest/reg.php
@@ -2,10 +2,9 @@
 session_start();
 $_SESSION['message'] = '';
 $mysqli = new mysqli('localhost','root','','regusers');
-
 //the form has been submitted with post
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    
+  
     //two passwords are equal to each other
     if ($_POST['password'] == $_POST['confirmpassword']) {
         
@@ -16,22 +15,29 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         //md5 and post variable for password 
         $password = md5($_POST['password']); 
         //post variable for avatar
-        //store files to images folder
+       
+       //store files to images folder
         $avatar_path = $mysqli->real_escape_string('images/'.$_FILES['avatar']['name']);
-
     
+    //do we need sessions to use for server side? 
 }
 
-
+//end php 
+?> 
+<!-- added background image and linked the css to reg.php-->
+<link href="http://more-sky.com/data/out/9/IMG_352169.jpg" rel="stylesheet" type="text/css"/> 
 <link rel="stylesheet" href="registrationform.css" type="text/css">
 <div class="body-content">
   <div class="module">
     <h1>Welcome! Register Now!</h1>
-    <form class="form" action="registrationform.php" method="post" enctype="multipart/form-data" autocomplete="off">
+    <!-- had to make them 'required' also action was reg.php not registrationform.php-->
+    <form class="form" action="reg.php" method="post" enctype="multipart/form-data" autocomplete="off">
       <div class="alert alert-error"><?= $_SESSION['message'] ?></div> 
-      <input type="text" placeholder="UserName" name="username" />
-      <input type="email" placeholder="Email" name="email" />
-      <input type="password" placeholder="Password" name="password" />  
+      <input type="text" placeholder="UserName" name="username" required />
+      <input type="email" placeholder="Email" name="email" required />
+      <!-- had to make a confirm password to display on registration page on website-->
+      <input type="password" placeholder="Password" name="password" autocomplete="new-password" required />
+      <input type="password" placeholder="Confirm Password" name="confirmpassword" autocomplete="new-password" required />
       <div class="avatar"><label>Select your avatar: </label><input type="file" name="avatar" accept="image/*" required /></div>
       <input type="submit" value="Register" name="register" class="btn btn-block btn-primary" />
     </form>
