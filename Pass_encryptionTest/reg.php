34ca980bcfc0ac772c9d1cc8b53757ac9872fe36
JGustavo11 <jgustavo11@mail.fresnostate.edu>
2017-03-29 11:05:21 -0700

    fixed minor errors. Should be fully functional
    added confirm password verification and other verification to it

--- Pass_encryptionTest/reg.php
+++ Pass_encryptionTest/reg.php
@@ -2,9 +2,10 @@
 session_start();
 $_SESSION['message'] = '';
 $mysqli = new mysqli('localhost','root','','regusers');
+
 //the form has been submitted with post
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  
+    
     //two passwords are equal to each other
     if ($_POST['password'] == $_POST['confirmpassword']) {
         
@@ -15,19 +16,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         //md5 and post variable for password 
         $password = md5($_POST['password']); 
         //post variable for avatar
-       
-       //store files to images folder
+        //store files to images folder
         $avatar_path = $mysqli->real_escape_string('images/'.$_FILES['avatar']['name']);
-    
-    //do we need sessions to use for server side? //check to see if image is the same type 
+        //check to see if image is the same type 
         if (preg_match("!image!",$_FILES['avatar']['type'])) {
             
             //copy images to $avatar_path and the path is (('images/'.$_FILES['avatar']['name']))
             if (copy($_FILES['avatar']['tmp_name'], $avatar_path)){
                 
                 //session variables to be used on server 
-                $_SESSION['username'] = username;
-                $_SESSION['avatar'] = avatar_path;
+                $_SESSION['username'] = $username;
+                $_SESSION['avatar'] = $avatar_path;
 
                 //insert user data into database
                 $sql = "INSERT INTO accounts (username, email, password, avatar) "
@@ -39,30 +38,36 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     $_SESSION['message'] = "Registration succesful! $username has registered, Welcome!";
                     header("location: member.php");
                 }
-                // need a fail case
-                $mysqli->open();          
+                //however if failed, display error message that account couldnt be made 
+                else {
+                    $_SESSION['message'] = 'User could not be added to the database!';
+                }
+                //end
+                $mysqli->close();          
+            }//dispaly error message if file type not the same
+            else {
+                $_SESSION['message'] = 'File upload failed!';
             }
         }
-        
+        else {
+            $_SESSION['message'] = 'This isnt a picture!';
+        }
     }
-    // need to check if passwords are similar
-	
+    //check for that both are same passwords, if not, dispaly error 
+    else {
+        $_SESSION['message'] = 'Two passwords do not match!';
+    }
 }
-
-//end php 
-?> 
-<!-- added background image and linked the css to reg.php-->
-<link href="http://more-sky.com/data/out/9/IMG_352169.jpg" rel="stylesheet" type="text/css"/> 
+?>
+<link href="http://more-sky.com/data/out/9/IMG_352169.jpg" rel="stylesheet" type="text/css"/>
 <link rel="stylesheet" href="registrationform.css" type="text/css">
 <div class="body-content">
   <div class="module">
     <h1>Welcome! Register Now!</h1>
-    <!-- had to make them 'required' also action was reg.php not registrationform.php-->
-    <form class="form" action="reg.php" method="post" enctype="multipart/form-data" autocomplete="off">
+    <form class="form" action="registrationform.php" method="post" enctype="multipart/form-data" autocomplete="off">
       <div class="alert alert-error"><?= $_SESSION['message'] ?></div> 
       <input type="text" placeholder="UserName" name="username" required />
       <input type="email" placeholder="Email" name="email" required />
-      <!-- had to make a confirm password to display on registration page on website-->
       <input type="password" placeholder="Password" name="password" autocomplete="new-password" required />
       <input type="password" placeholder="Confirm Password" name="confirmpassword" autocomplete="new-password" required />
       <div class="avatar"><label>Select your avatar: </label><input type="file" name="avatar" accept="image/*" required /></div>
