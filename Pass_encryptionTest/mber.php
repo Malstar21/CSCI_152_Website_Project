e29314957d2cd698ab6ad51ed809227a703b9045
elvin-rivera23 <elvin_rivera@mail.fresnostate.edu>
2017-04-03 11:46:59 -0700

    reworked the mber php file and added better features, still missing comments and error fixes as well as some more functionality

--- Pass_encryptionTest/mber.php
+++ Pass_encryptionTest/mber.php
@@ -1,25 +1,29 @@
-<?php
-session_start();
-if(isset($_SESSION['user']))
-{
-?>
-<!doctype html>
-<html>
-<head>
-<title>Member</title>
-</head>
-<body>
-<?php echo "Welcome, ".$_SESSION['user']."!";?>
-<br />
-<h3>Welcome to the member aread!</h3>
-<p><a href="logout.php">Logout</a></p>
-</body>
-</html>
-<?php
-}
-else
-{
-echo "Please Login First! <a href='login.php'>Login</a>";
+
+<link rel="stylesheet" href="form.css">
+<?php session_start(); ?>
+<div class="body content">
+    <div class="welcome">
+        <div class="alert alert"><?= $_SESSION[''] ?></div>
+        <img src="<?= $_SESSION['avatar'] ?>"><br />
+        Welcome <span class="user"><?= $_SESSION['user'] ?></span>
+        <?php
+        $mysqli = new mysqli("localhost", "root", "", "accounts_complete");
+       
+        $sql = "SELECT username, avatar FROM accounts";
+        $result = $mysqli->query(sql); 
     
-}
-?>
+        ?>
+  
+        <span>All registered users:</span>
+        <?php
+        while($row = $result->fetch_assoc()){ 
+            echo '<pre>';
+            print(row);
+            echo '</pre>';
+            echo "<div class='userlist'><span>$row[user]</span><br />";
+            echo "<img src='$row[avatar]'></div>";
+        }
+        ? 
+        </div>
+    </div>
+</div>
