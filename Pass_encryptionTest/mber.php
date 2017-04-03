7e81bbf2ab11c6031c9f43746923d1d51b32531c
JGustavo11 <jgustavo11@mail.fresnostate.edu>
2017-04-03 11:57:33 -0700

    php not working

--- Pass_encryptionTest/mber.php
+++ Pass_encryptionTest/mber.php
@@ -3,27 +3,24 @@
 <?php session_start(); ?>
 <div class="body content">
     <div class="welcome">
-        <div class="alert alert"><?= $_SESSION[''] ?></div>
+        <div class="alert alert-success"><?= $_SESSION['message'] ?></div>
         <img src="<?= $_SESSION['avatar'] ?>"><br />
-        Welcome <span class="user"><?= $_SESSION['user'] ?></span>
+        Welcome <span class="user"><?= $_SESSION['username'] ?></span>
         <?php
         $mysqli = new mysqli("localhost", "root", "", "accounts_complete");
-       
         $sql = "SELECT username, avatar FROM accounts";
-        $result = $mysqli->query(sql); 
-    
+        $result = $mysqli->query($sql); 
         ?>
-  
+        <div id='registered'>
         <span>All registered users:</span>
         <?php
         while($row = $result->fetch_assoc()){ 
-            echo '<pre>';
-            print(row);
-            echo '</pre>';
-            echo "<div class='userlist'><span>$row[user]</span><br />";
+            //echo '<pre>';
+            //print_r($row);
+            //echo '</pre>';
+            echo "<div class='userlist'><span>$row[username]</span><br />";
             echo "<img src='$row[avatar]'></div>";
         }
-        ? 
         </div>
     </div>
 </div>
