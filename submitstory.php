ff7c084753fafd080ef6ff9dc559ccb40a7067f0
Malstar21 <malek.02@hotmail.com>
2017-05-03 11:52:25 -0700

    making story now links to user's acccount

--- submitstory.php
+++ submitstory.php
@@ -1,17 +1,18 @@
 <?php
+	session_start();
 	$dbhost = "localhost";
 	$user = "root";
 	$password = "";
 	$db = "seem website";
-	
+
 	$conn = mysql_connect($dbhost, $user, $password);
 	mysql_select_db($db);
-	
+
 	if ($conn == false) {
 		die("Connection failed");
 	}
 	echo "connected";
-	
+
 	if(isset($_POST['submit'])) {
 		$insertTitle = mysql_real_escape_string($_POST['title']);
 		$insertStory = mysql_real_escape_string($_POST['comment']);
@@ -19,8 +20,11 @@
 		$image_name = addslashes($_FILES['image']['name']);
 		$image = file_get_contents($image);
 		$image = base64_encode($image);
-		$sql = "INSERT INTO makeastory (story,image,imagename, title) VALUES ('$insertStory', '$image', '$image_name', '$insertTitle') ";
-		
+
+		$key = $_SESSION['userID'];	// get user's ID
+
+		$sql = "INSERT INTO makeastory (story,image,imagename, title, userKey) VALUES ('$insertStory', '$image', '$image_name', '$insertTitle', '$key') ";
+
 		if(mysql_query($sql)) {
 			echo "Records added";
 		}
